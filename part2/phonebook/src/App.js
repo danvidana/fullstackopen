c5a4128d745cef717f21de6e3d080bb550329baa
danvidana <32367237+danvidana@users.noreply.github.com>
2022-09-08 17:22:22 -0500

    Finished up to ex 2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,112 @@
+import { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <div>{person.name} {person.number}</div>
+  )
+}
+
+const Filter = ({ searchTerm, handler }) => {
+  return (
+    <div>
+      filter shown with <input value={searchTerm} onChange={handler} />
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.addPerson}>
+      <div>
+        name: <input value={props.newName} onChange={props.handleNameChange} />
+      </div>
+      <div>
+        number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map(person =>
+        <Person key={person.name} person={person} />
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filtering, setFiltering] = useState(false)
+  const [newSearch, setNewSearch] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(persons.filter(person => person.name === newName).length > 0) {
+      alert(`${newName} is already added to phonebook`)
+    }else {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    if(event.target.value === '') {
+      setFiltering(false)
+    }else {
+      setFiltering(true)
+    }
+
+    setNewSearch(event.target.value)
+  }
+
+  const personsToShow = filtering
+    ? persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+    : persons
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        searchTerm={newSearch}
+        handler={handleSearchChange} />
+      <h2>add a new</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange} />
+      <h2>Numbers</h2>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+export default App
