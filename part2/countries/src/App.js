824f89ec3e3207a2b9687d10ff3b89a6a8ac28cf
danvidana <32367237+danvidana@users.noreply.github.com>
2022-09-27 14:08:48 -0500

    Finished exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,10 +9,76 @@ const Filter = ({ searchTerm, handler }) => {
   )
 }
 
+const Country = ({ name, buttonHandler }) => {
+  return (
+    <div>
+    {name}
+    <button onClick={() => buttonHandler(name)} >
+      show
+    </button>
+    </div>
+  )
+}
+
+const Languages = ({ languages }) => {
+  return (
+    <ul>
+        {Object.values(languages).map(language =>
+            <li key={language}> {language} </li>
+          )
+        }
+    </ul>
+  )
+}
+
+const WeatherSection = ({country}) => {
+  return (
+    <div>
+      <h2>Weather in {country.capital}</h2>
+      <div>temperature {}</div>
+    </div>
+  )
+}
+
+const CountryDetails = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h3>languages:</h3>
+      <Languages languages={country.languages} />
+      <img src={country.flags.png} alt={`flag of ${country.name.common}`} />
+      <WeatherSection country={country} />
+    </div>
+  )
+}
+
+const CountriesList = ({ countries, buttonHandler }) => {
+  if(countries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  }else if(countries.length > 1) {
+    return (
+      <div>
+        {countries.map(country =>
+          <Country key={country.name.common} name={country.name.common} buttonHandler={buttonHandler}/>
+        )}
+      </div>
+    )
+  }else if(countries.length === 1) {
+    return (
+      <CountryDetails country={countries[0]}/>
+    )
+  }
+}
+
 function App() {
   const [newSearch, setNewSearch] = useState('')
   const [filtering, setFiltering] = useState(false)
-  const [countriesCount, setCountriesCount] =  useState(0)
   const [allCountriesData, setAllCountriesData] = useState([])
 
   const hook = () => {
@@ -37,7 +103,10 @@ function App() {
     setNewSearch(event.target.value)
   }
 
-  console.log(allCountriesData)
+  const handleShowButton = (name) => {
+    console.log("handleShowButton", name)
+    setNewSearch(name)
+  }
 
   const countriesToShow = filtering
     ? allCountriesData.filter(country => country.name.common.toLowerCase().includes(newSearch.toLowerCase()))
@@ -49,6 +118,7 @@ function App() {
         searchTerm={newSearch}
         handler={handleSearchChange}
       />
+      <CountriesList countries={countriesToShow} buttonHandler={handleShowButton} />
     </div>
   );
 }
