eae9350a4430be46d282dddad62efff8d5d8c315
danvidana <32367237+danvidana@users.noreply.github.com>
2022-11-28 16:24:56 -0600

    Finished Exercise 3.22

--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an agrument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an agrument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -10,36 +10,35 @@ const password = process.argv[2]
 const url = `mongodb+srv://danvidana:${password}@cluster0.5sr6vxs.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected')
+  .connect(url)
+  .then(() => {
+    console.log('connected')
 
-        // const note = new Note({
-        //     content: 'Test 3',
-        //     date: new Date(),
-        //     important: true,
-        // })
-
-        // return note.save()
-
-        Note.find({}).then(result => {
-            result.forEach(note =>  {
-                console.log(note)
-            })
-            mongoose.connection.close()
-        })
-        
-    })
-    // .then(() => {
-    //     console.log('note saved!')
-    //     return mongoose.connection.close()
+    // const note = new Note({
+    //     content: 'Test 3',
+    //     date: new Date(),
+    //     important: true,
     // })
-    .catch((err) => console.log(err))
+
+    // return note.save()
+
+    Note.find({}).then(result => {
+      result.forEach(note =>  {
+        console.log(note)
+      })
+      mongoose.connection.close()
+    })
+  })
+// .then(() => {
+//     console.log('note saved!')
+//     return mongoose.connection.close()
+// })
+  .catch((err) => console.log(err))
