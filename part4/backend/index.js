2fdc93fc7447f2033c4964dc9b4b73dcf1e1e328
danvidana <32367237+danvidana@users.noreply.github.com>
2022-11-30 14:36:02 -0600

    Finished right before Exercise 4.3

---
+++ part4/backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/notes', notesRouter)
+app.use(express.static('build'))
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
---
+++ part4/backend/models/note.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
+})
+
+//Changes _id Mongo property into "id"
+//Removes __v Mongo property
+noteSchema.set('toJSON',  {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
