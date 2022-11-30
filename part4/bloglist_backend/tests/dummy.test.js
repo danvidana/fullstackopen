9e6e44478ecbb8481d5e778b9f293ca67035d9c3
danvidana <32367237+danvidana@users.noreply.github.com>
2022-11-30 14:54:45 -0600

    Finished Exercise 4.4

---
+++ part4/bloglist_backend/tests/dummy.test.js
@@ -0,0 +1,79 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '7b422aa4o454a676234d17f8',
+      title: 'Quantum computing is the future',
+      author: 'John H. Hamlin',
+      url: 'http://www.arizona.tec.edu',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    expect(listHelper.totalLikes(emptyList)).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.totalLikes(listWithMultipleBlogs)).toBe(24)
+  })
+
+})
