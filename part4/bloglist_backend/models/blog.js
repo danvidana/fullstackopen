522f3e4635127fdcec0c219ed36a10446711ea70
danvidana <32367237+danvidana@users.noreply.github.com>
2022-11-30 13:57:14 -0600

    Finished Exercise 4.1

---
+++ part4/bloglist_backend/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    likes: Number
+})
+
+//Changes _id Mongo property into "id"
+//Removes __v Mongo property
+blogSchema.set('toJSON',  {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
