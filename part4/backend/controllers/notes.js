b244edd2a6dde153d9565470ca66fe60e3105548
danvidana <32367237+danvidana@users.noreply.github.com>
2022-12-01 10:38:17 -0600

    Finished before Exercise 4.8

--- part4/backend/controllers/notes.js
+++ part4/backend/controllers/notes.js
@@ -2,33 +2,28 @@ const notesRouter = require('express').Router()
 
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if(note) {
-        response.json(note)
-      }else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+//GET a single note
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if(note) {
+    response.json(note)
+  }else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+//DELETE a single note
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -37,11 +32,8 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date(),
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.put('/:id', (request, response, next) => {
--- part4/backend/index.js
+++ part4/backend/index.js
@@ -1,31 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/notes')
+const logger = require('./utils/logger')
 
-logger.info('connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/notes', notesRouter)
-app.use(express.static('build'))
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
