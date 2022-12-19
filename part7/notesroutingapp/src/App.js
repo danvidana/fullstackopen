17da323693d9ba7b95f07138cd7bff8041381a41
danvidana <32367237+danvidana@users.noreply.github.com>
2022-12-19 16:45:24 -0600

    In 7.c Styled components

--- part7/notesroutingapp/src/App.js
+++ part7/notesroutingapp/src/App.js
@@ -1,4 +1,19 @@
 import { useState } from 'react'
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
+  Alert,
+  AppBar,
+  Toolbar,
+  IconButton
+} from '@mui/material'
 
 import {
   Routes,
@@ -30,13 +45,23 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map(note => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>
+                {note.user}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 )
 
@@ -65,12 +90,16 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          <TextField label="username" />
         </div>
         <div>
-          password: <input type='password' />
+          <TextField label="password" type='password' />
         </div>
-        <button type="submit">login</button>
+        <div>
+          <Button variant="contained" color="primary" type="submit">
+            login
+          </Button>
+        </div>
       </form>
     </div>
   )
@@ -104,9 +133,14 @@ const App = () => {
     : null
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
   }
 
   const padding = {
@@ -115,16 +149,34 @@ const App = () => {
 
 
   return (
-    <div>
-        <div>
-          <Link style={padding} to="/">home</Link>
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
-        </div>
+    <Container>
+      <div className='container' >
+        {(message &&
+          <Alert serverity="success">
+            {message}
+          </Alert>
+        )}
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+            </IconButton>
+            <Button color="inherit" component={Link} to="/">
+              home
+            </Button>
+            <Button color="inherit" component={Link} to="/notes">
+              notes
+            </Button>
+            <Button color="inherit" component={Link} to="/users">
+              users
+            </Button>
+              {user
+                ? <em>{user} logged in</em>
+                : <Button color="inherit" component={Link} to="/login">
+                  login
+                </Button>
+              }              
+          </Toolbar>
+        </AppBar>
 
         <Routes>
           <Route path="/notes/:id" element={<Note note={note} />} />
@@ -133,11 +185,12 @@ const App = () => {
           <Route path="/login" element={<Login onLogin={login} />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      <div>
-        <br />
-        <em>Note app, Department of Computer Science 2022</em>
+        <div>
+          <br />
+          <em>Note app, Department of Computer Science 2022</em>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
