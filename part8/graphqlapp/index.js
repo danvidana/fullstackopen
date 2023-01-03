a1b332c4023878a2ccc80b7dbceadcd464c72756
danvidana <32367237+danvidana@users.noreply.github.com>
2023-01-03 11:27:31 -0600

    Finished Exercise 8.7

--- part8/graphqlapp/index.js
+++ part8/graphqlapp/index.js
@@ -35,9 +35,13 @@ const typeDefs = gql`
     address: Address!
     id: ID!
   }
+  enum YesNo {
+    YES
+    NO
+  }
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
   type Mutation {
@@ -47,15 +51,27 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) =>{
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
+  
   Person: {
     address: ({ street, city }) => {
       return {
@@ -64,6 +80,7 @@ const resolvers = {
       }
     },
   },
+
   Mutation: {
     addPerson: (root, args) => {
       if (persons.find((p) => p.name === args.name)) {
@@ -71,11 +88,20 @@ const resolvers = {
           invalidArgs: args.name,
         })
       }
-
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
     },
+
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if(!person) {
+        return null
+      }
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }
   },
 }
 
